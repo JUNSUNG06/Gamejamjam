f762c1515d76b50a722aae170d8dc1f58d74eaf3
hueam <zminsu5@gmail.com>
2022-08-20 03:41:33 +0900

    yami

--- Assets/01.Scripts/Class/Turtle.cs
+++ Assets/01.Scripts/Class/Turtle.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[System.Serializable]
 public class Turtle : MonoBehaviour
 {
+    [SerializeField]char _grade;
+    string _name;
+    int _helth;
+    string _des;
+    int _price;
+    int _prob;
+    public string Name{get => _name;}
+    public Turtle(string grade, string name, int helth, string des, int price, int prob){
+
+    }
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/01.Scripts/Core/GameManager.cs
+++ Assets/01.Scripts/Core/GameManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField]List<Turtle> turtles = new List<Turtle>();
     [SerializeField] private List<PoolableMono> poolObj = new List<PoolableMono>();
     [SerializeField] private int poolCount = 0;
 
     private void Awake() 
-    {    
+    {   
         PoolManager.Instance = new PoolManager(this.transform);
         
         foreach(PoolableMono item in poolObj)
